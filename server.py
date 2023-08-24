44e7090c7a8e0b252f05e397581d321b4fa6362d
MannsWrld <116054062+MannsWrld@users.noreply.github.com>
2023-08-24 03:02:25 -0700

    updated a lot

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask_app import app
 from flask_app.controllers import controller #replace rename_controller with your controller file name. Do not include .py
+from flask_app.controllers import records
+from flask_app.controllers import hospitals
 
 if __name__=='__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
